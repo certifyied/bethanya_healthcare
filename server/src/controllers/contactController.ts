2fce8254be1119faef4716660447bf50035b0e45
certifyied <soorajcpherupunna2@gmail.com>
2026-05-07 21:27:23 +0530

    updated

--- server/src/controllers/contactController.ts
+++ server/src/controllers/contactController.ts
@@ -94,12 +94,18 @@ export const sendMessage = async (
 
     // ✅ Nodemailer Transport
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
+
     await transporter.verify();
     console.log("SMTP READY");
 
